9475cfd4c7289b6472ddf1ec5b8cfe35b83963f0
Mark <m_arch@outlook.com.au>
2019-07-02 12:15:38 +1000

    App ready to start implementing live chatroom functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   validates :username, presence: true,
    length: { minimum: 4, maximum: 12 },
    uniqueness: { case_sensitive: false }
-
+  has_many :messages
   has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#home'
   get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+  post 'message' => 'messages#create'
 
 end
